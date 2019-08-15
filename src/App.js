2c49e1cf951f09d6357b59437e36cc05efb36736
Ang Xu <angxu1234@gmail.com>
2019-08-15 15:50:01 -0500

    :package: Package
    Installing initial pacakges and analytics #1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import createHistory from "history/createBrowserHistory";
+import "./App.css";
+
+const history = createHistory();
+ReactGA.initialize("UA-145774968-0");
+history.listen((location, action) => {
+	ReactGA.pageview(location.pathname + location.search);
+	console.log(location.pathname);
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Welcome to Allegiance
-      </header>
-    </div>
-  );
+	return (
+		<Router history={history}>
+			<div className="App">
+				<header className="App-header">Welcome to Allegiance</header>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
