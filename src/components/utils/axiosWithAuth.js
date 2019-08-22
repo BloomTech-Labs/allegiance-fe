2f959af1a0c8cb6eab1033a4882ca44b5c1e12ec
Ang Xu <angxu1234@gmail.com>
2019-08-22 12:20:09 -0500

    changed axios routes to point to staging heroku

--- src/components/utils/axiosWithAuth.js
+++ src/components/utils/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const axiosWithAuth = token => {
-  return axios.create({
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    baseURL: "http://localhost:5000/api"
-    // baseURL: "https://labs15-allegiance-staging.herokuapp.com/api"
-    // baseURL: "https://labs15-allegiance.herokuapp.com/api"
-  });
+	return axios.create({
+		headers: {
+			Authorization: `Bearer ${token}`
+		},
+		// baseURL: "http://localhost:5000/api"
+		baseURL: "https://labs15-allegiance-staging.herokuapp.com/api"
+		// baseURL: "https://labs15-allegiance.herokuapp.com/api"
+	});
 };
