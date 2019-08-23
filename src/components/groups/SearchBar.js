3622f2b71122783a34092e75f64a28772a021db4
Ang Xu <angxu1234@gmail.com>
2019-08-23 18:06:00 -0500

    key up and down working using component in same js file, need to refactor to work when data is on multiple components

--- src/components/groups/SearchBar.js
+++ src/components/groups/SearchBar.js
@@ -1,3 +1,114 @@
+// import React, { useState, useEffect } from "react";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
+// import useGetToken from "../utils/useGetToken";
+// import useForm from "../utils/useForm";
+// import useDebounce from "../utils/useDebounce";
+
+// import "../../App.scss";
+
+// import SearchResults from "./SearchResults";
+
+// const SearchBar = () => {
+// 	// useStates for results and is searching status
+// 	const [results, setResults] = useState([]);
+// 	const [isSearching, setIsSearching] = useState(false);
+// 	// token for accessing authentication required backend routes
+// 	const [token] = useGetToken();
+// 	// useForm custom hook and set timeout custom hook
+// 	const { values, setValues, handleChange, handleSubmit } = useForm(fillSearch);
+// 	const debouncedSearchTerm = useDebounce(values.group_name, 1000);
+// 	// useStates for handling up and down arrow key selections
+// 	const [activeSuggestion, setSuggestion] = useState(0);
+
+// 	// callback function to handle submit
+// 	function fillSearch(e, group) {
+// 		if (group) {
+// 			setValues({ group_name: group.group_name });
+// 		}
+// 	}
+
+// 	// Handle up and down arrow keys
+// 	const onKeyDown = e => {
+// 		// User pressed the enter key
+// 		if (e.keyCode === 13) {
+// 			setSuggestion(0);
+// 			setValues(results[activeSuggestion]);
+// 		}
+// 		// User pressed the up arrow
+// 		if (e.keyCode === 38) {
+// 			if (activeSuggestion === 0) {
+// 				return;
+// 			}
+// 			setSuggestion(activeSuggestion - 1);
+// 		}
+// 		// User pressed the down arrow
+// 		else if (e.keyCode === 40) {
+// 			if (activeSuggestion - 1 === results.length) {
+// 				return;
+// 			}
+// 			setSuggestion(activeSuggestion + 1);
+// 		}
+// 	};
+
+// 	//useEffect to grab groups that are searched for from the backend (column and row filters for only group results that are being searched)
+// 	useEffect(() => {
+// 		const fetchData = async () => {
+// 			const groups = await axiosWithAuth([token]).post(
+// 				"https://labs15-allegiance-staging.herokuapp.com/api/groups/search",
+// 				// "http://localhost:5000/api/groups",
+// 				{
+// 					column: "group_name",
+// 					row: values.group_name
+// 				}
+// 			);
+// 			return groups;
+// 		};
+// 		// If empty string in search immediately set results array to blank
+// 		if (values.group_name === "") setResults([]);
+// 		// Make sure we have a value (user has entered something in input)
+// 		else if (debouncedSearchTerm) {
+// 			// Set isSearching state
+// 			setIsSearching(true);
+// 			// Fire off our API call
+// 			fetchData().then(res => {
+// 				// Set searching state
+// 				setIsSearching(false);
+// 				// Set results state
+// 				setResults(res.data.groupByFilter);
+// 			});
+// 		}
+// 	}, [values, token, debouncedSearchTerm]);
+
+// 	console.log(values);
+// 	console.log(results);
+// 	console.log(isSearching);
+
+// 	return (
+// 		<div className="search-form">
+// 			<form className="form-items" onSubmit={handleSubmit}>
+// 				<label className="label">Search</label>
+// 				<input
+// 					onChange={handleChange}
+// 					onKeyDown={onKeyDown}
+// 					name="group_name"
+// 					id="group_name"
+// 					placeholder="Enter Group Name"
+// 					value={values.group_name || ""}
+// 					type="text"
+// 				/>
+// 			</form>
+// 			<SearchResults
+// 				results={results}
+// 				fillSearch={fillSearch}
+// 				onKeyDown={onKeyDown}
+// 			/>
+// 		</div>
+// 	);
+// };
+
+// export default SearchBar;
+
+//Testing for arrow key up and down, need to refactor to use multiple components
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import useGetToken from "../utils/useGetToken";
@@ -5,8 +116,6 @@ import useForm from "../utils/useForm";
 import useDebounce from "../utils/useDebounce";
 
 import "../../App.scss";
-
-import SearchResults from "./SearchResults";
 
 const SearchBar = () => {
 	// useStates for results and is searching status
@@ -19,12 +128,36 @@ const SearchBar = () => {
 	const debouncedSearchTerm = useDebounce(values.group_name, 1000);
 	// useStates for handling up and down arrow key selections
 	const [activeSuggestion, setSuggestion] = useState(0);
+
 	// callback function to handle submit
 	function fillSearch(e, group) {
 		if (group) {
 			setValues({ group_name: group.group_name });
 		}
 	}
+
+	// Handle up and down arrow keys
+	const onKeyDown = e => {
+		// User pressed the enter key
+		if (e.keyCode === 13) {
+			setSuggestion(0);
+			setValues(results[activeSuggestion]);
+		}
+		// User pressed the up arrow
+		if (e.keyCode === 38) {
+			if (activeSuggestion === 0) {
+				return;
+			}
+			setSuggestion(activeSuggestion - 1);
+		}
+		// User pressed the down arrow
+		else if (e.keyCode === 40) {
+			if (activeSuggestion - 1 === results.length) {
+				return;
+			}
+			setSuggestion(activeSuggestion + 1);
+		}
+	};
 
 	//useEffect to grab groups that are searched for from the backend (column and row filters for only group results that are being searched)
 	useEffect(() => {
@@ -55,11 +188,32 @@ const SearchBar = () => {
 		}
 	}, [values, token, debouncedSearchTerm]);
 
-	// Handle up and down arrow keys
-
 	console.log(values);
 	console.log(results);
 	console.log(isSearching);
+
+	let suggestionsListComponent;
+
+	if (results.length) {
+		suggestionsListComponent = (
+			<div className="results-container">
+				{results.map((group, index) => {
+					let className;
+
+					// Flag the active suggestion with a class
+					if (index === activeSuggestion) {
+						className = "suggestion-active";
+					}
+
+					return (
+						<li className={className} key={group.id} onClick={handleSubmit}>
+							{group.group_name}
+						</li>
+					);
+				})}
+			</div>
+		);
+	}
 
 	return (
 		<div className="search-form">
@@ -67,6 +221,7 @@ const SearchBar = () => {
 				<label className="label">Search</label>
 				<input
 					onChange={handleChange}
+					onKeyDown={onKeyDown}
 					name="group_name"
 					id="group_name"
 					placeholder="Enter Group Name"
@@ -74,7 +229,7 @@ const SearchBar = () => {
 					type="text"
 				/>
 			</form>
-			<SearchResults results={results} fillSearch={fillSearch} />
+			{suggestionsListComponent}
 		</div>
 	);
 };
