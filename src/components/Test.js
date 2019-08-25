a3942c2f12e6856d9293f0deedd7fd2b7c936486
Ang Xu <angxu1234@gmail.com>
2019-08-25 18:11:51 -0500

    add token console log so team can test postman/test DB with bearer token

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from "react";
 
-import { axiosWithAuth } from "./utils/axiosWithAuth"
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 import useGetToken from "./utils/useGetToken";
 
 import { Mixpanel } from "./analytics/Mixpanel";
 
 const Test = () => {
+	const [data, setData] = useState({ users: [] });
 
-  const [data, setData] = useState({ users: [] });
+	useEffect(() => {
+		Mixpanel.track("Hello mixpanel!");
+	}, []);
 
-  useEffect(() => {
-    Mixpanel.track("Hello mixpanel!");
-  }, []);
+	const [token] = useGetToken();
 
-  const [token] = useGetToken()
+	console.log(token);
 
-  useEffect(() => {
-    const fetchData = async () => {
+	useEffect(() => {
+		const fetchData = async () => {
+			const users = await axiosWithAuth([token]).get("/users");
 
-      const users = await axiosWithAuth([token]).get('/users')
+			setData(users.data);
+		};
+		fetchData();
+	}, [token]);
 
-      setData(users.data)
-    };
-    fetchData();
-  }, [token]);
+	console.log(data);
 
-  console.log(data);
-
-  return (
-    <div>
-      {data.users.map(user => (
-        <div key={user.id}>
-          {user.username}, {user.email}, {user.location}
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div>
+			{data.users.map(user => (
+				<div key={user.id}>
+					{user.username}, {user.email}, {user.location}
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Test;
