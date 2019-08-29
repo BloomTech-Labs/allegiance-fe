ef8dee9c442cf6589182a15f5a4a50603bdfc907
Ang Xu <angxu1234@gmail.com>
2019-08-29 06:02:22 -0500

    :bug: Minor error message fixes
    Fixing error messages from console when navigating through app, no major changes

--- src/components/groups/SearchResults.js
+++ src/components/groups/SearchResults.js
@@ -6,33 +6,33 @@ import "../../App.scss";
 
 // Need to refine search so it doesn't return every result, cap at x number initially
 const SearchResults = props => {
-  return (
-    <div className="results-container">
-      {/* bring activeSuggestion number from SearchBar, format entry with suggestion-active class */}
-      {props.results.map((group, index) => {
-        let className;
-        if (index === props.activeSuggestion) {
-          className = "suggestion-active";
-        }
-        return (
-          // onClick or enter key calls fillSearch from SearchBar
+	return (
+		<div className="results-container">
+			{/* bring activeSuggestion number from SearchBar, format entry with suggestion-active class */}
+			{props.results.map((group, index) => {
+				let className;
+				if (index === props.activeSuggestion) {
+					className = "suggestion-active";
+				}
+				return (
+					// onClick or enter key calls fillSearch from SearchBar
 
-          <Link to={`/group/${group.id}`}>
-            <div key={group.id} className={`single-result ${className}`}>
-              <ResultImage src={group.image} />{" "}
-              <div className="result-info">{group.group_name}</div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
+					<Link to={`/group/${group.id}`} key={group.id}>
+						<div className={`single-result ${className}`}>
+							<ResultImage src={group.image} />{" "}
+							<div className="result-info">{group.group_name}</div>
+						</div>
+					</Link>
+				);
+			})}
+		</div>
+	);
 };
 
 const ResultImage = styled.img`
-  max-width: 15%;
-  height: auto;
-  border-radius: 50%;
+	max-width: 15%;
+	height: auto;
+	border-radius: 50%;
 `;
 
 export default SearchResults;
