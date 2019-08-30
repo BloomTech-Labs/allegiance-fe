32ba54877351c1242b9ec1a99ef185359f2429f4
JarredStanford <stanj89@gmail.com>
2019-08-30 12:30:19 -0400

    mobile landing page ready

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Image } from 'semantic-ui-react'
 import styled from 'styled-components'
 import { useAuth0 } from "./auth/react-auth0-wrapper";
+import Logo from "../assets/Logo.png"
+import LandingImage from "../assets/LandingImage.jpg"
 
 const Landing = () => {
     const { loginWithRedirect } = useAuth0();
     return (
         <LandingContainer>
-            <Headers>Welcome to Allegiance</Headers>
+            <LogoHolder>
+                <Image src={Logo} size='small' />
+                <h1>ALLEGIANCE</h1>
+            </LogoHolder>
             <Headers>The social network for sports fans</Headers>
             <ButtonHolder>
                 <Button
@@ -26,11 +31,18 @@ const LandingContainer = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: space-evenly;
-        background-color: black;
+        background-image: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.5)), url(${LandingImage});
+        background-size: cover;
         color: white;
         height: 100vh;
         margin-top: -15%;
         align-items: center;`
+
+const LogoHolder = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;`
+
 
 const Headers = styled.h1`
         width: 75%;
