cda851df842e88c1527c2e4ca36227b2c589be26
danpatrickoneill <dan47573@gmail.com>
2019-08-30 08:10:01 -0400

    Added nearby groups to group container

--- src/components/groups/GroupContainer.js
+++ src/components/groups/GroupContainer.js
@@ -7,51 +7,54 @@ import styled from "styled-components";
 import GroupList from "./GroupList";
 import SearchBar from "./SearchBar";
 import MyAllegianceGroups from "../profile/MyAllegianceGroups";
+import NearbyGroups from "./NearbyGroups";
 
 function GroupContainer() {
-	const loggedInGroups = useSelector(state => state.userReducer.loggedInGroups);
+  const loggedInGroups = useSelector(state => state.userReducer.loggedInGroups);
 
-	return (
-		<div>
-			<SearchBar />
-			<Divider />
-			<MyGroups>
-				<GroupTitleHolder>
-					<H3>MY GROUPS</H3>
-					<Link to="/creategroup">
-						<Popup
-							content="Create a Group"
-							trigger={<Icon name="plus square" size="small" />}
-						/>
-					</Link>
-				</GroupTitleHolder>
-				<>
-					<MyAllegianceGroups content={loggedInGroups} />
-				</>
-			</MyGroups>
-			<Divider />
-			<GroupList />
-		</div>
-	);
+  return (
+    <div>
+      <SearchBar />
+      <Divider />
+      <MyGroups>
+        <GroupTitleHolder>
+          <H3>MY GROUPS</H3>
+          <Link to="/creategroup">
+            <Popup
+              content="Create a Group"
+              trigger={<Icon name="plus square" size="small" />}
+            />
+          </Link>
+        </GroupTitleHolder>
+        <>
+          <MyAllegianceGroups content={loggedInGroups} />
+        </>
+      </MyGroups>
+      <Divider />
+      <NearbyGroups />
+      <Divider />
+      <GroupList />
+    </div>
+  );
 }
 export default GroupContainer;
 
 const MyGroups = styled.div`
-	margin: 2% 5%;
+  margin: 2% 5%;
 `;
 
 const GroupTitleHolder = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-	h3 {
-		margin: 0 1%;
-	}
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  h3 {
+    margin: 0 1%;
+  }
 `;
 
 const H3 = styled.h3`
-	font-size: 1rem;
-	font-weight: bold;
-	margin: 1% 0;
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 1% 0;
 `;
