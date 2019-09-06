1091da3f78277d611d3770749a4c2c908a7e72ef
JarredStanford <stanj89@gmail.com>
2019-09-06 14:27:05 -0400

    fixed logout, added coming soon pages.

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ComingSoon from '../../assets/ComingSoon.svg'
+import { Image } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const UnderConstruction = props => {
+    return (
+        <ConstructionDiv>
+            <h1>Feature Coming Soon!</h1>
+            <Image src={ComingSoon} />
+        </ConstructionDiv>
+    )
+}
+
+const ConstructionDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+height: 80vh
+width: 95%;
+margin: auto;`
+
+export default UnderConstruction
