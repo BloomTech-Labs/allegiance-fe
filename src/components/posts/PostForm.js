6fc62156bd82e6f4343caf40f26a7b5a47255d31
Derek S <dschwan21@me.com>
2019-09-12 00:03:47 -0700

    refactored semantic ui form and textbox into material ui texfield and button

--- src/components/posts/PostForm.js
+++ src/components/posts/PostForm.js
@@ -6,9 +6,14 @@ import useGetToken from "../utils/useGetToken";
 import useForm from "../utils/useForm";
 
 import styled from "styled-components";
-import { Form, Button, TextArea } from "semantic-ui-react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import { green } from "@material-ui/core/colors";
 
 const PostForm = props => {
+  const primary = green[500];
   // token for accessing authentication required backend routes
   const [token] = useGetToken();
 
@@ -32,28 +37,68 @@ const PostForm = props => {
     }
   }
 
-  return (
-    <FormContainer onSubmit={handleSubmit}>
-      <Form.Field
-        control={TextArea}
-        label="Post"
-        placeholder="Write a post to the group..."
-        onChange={handleChange}
-        name="post_content"
-        value={values.post_content || ""}
-      />
-      {/* <Form.Field control={Button}>Submit Post</Form.Field> */}
+  // Material UI
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: "flex",
+      flexWrap: "wrap"
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      backgroundColor: "white"
+    },
 
-      <Button type="submit">Submit</Button>
-    </FormContainer>
+    margin: {
+      margin: theme.spacing(1)
+    },
+    typography: {
+      fontSize: 13,
+      color: "black",
+      padding: 0
+    },
+    typography2: {
+      fontSize: 15,
+      color: "black",
+      fontWeight: "bold"
+    },
+    root: {
+      background: primary,
+      margin: theme.spacing(1)
+    }
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <form className={"reply-form"} onSubmit={handleSubmit}>
+      <div className={"input-div"}>
+        <TextField
+          id="outlined-textarea"
+          label="Post"
+          placeholder="Write a post to the group..."
+          multiline
+          fullWidth
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+          onChange={handleChange}
+          name="post_content"
+          value={values.post_content || ""}
+        />
+      </div>
+      <Fab classes={{ root: classes.root }} type="submit" aria-label="Reply">
+        <AddIcon />
+      </Fab>
+    </form>
   );
 };
 
-const FormContainer = styled(Form)`
-  margin-top: 5vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
+// const FormContainer = styled(Form)`
+//   margin-top: 5vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+// `;
 
 export default PostForm;
