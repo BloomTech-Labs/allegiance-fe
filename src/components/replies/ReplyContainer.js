fb54c9e0be62845ecbbe4dff58f09ba975268460
Ang Xu <angxu1234@gmail.com>
2019-09-13 06:11:45 -0500

    :bug: Big fixes
    Userreducer fix, replycontainer padding bottom fix

--- src/components/replies/ReplyContainer.js
+++ src/components/replies/ReplyContainer.js
@@ -18,147 +18,148 @@ import { green } from "@material-ui/core/colors";
 import ReplyCard from "./ReplyCard";
 
 const ReplyContainer = props => {
-  const [post, setPost] = useState();
-  const [submitted, setSubmitted] = useState(false);
-  const id = props.match.params.id;
-  const primary = green[500];
-  const userId = useSelector(state => state.userReducer.loggedInUser.id);
+	const [post, setPost] = useState();
+	const [submitted, setSubmitted] = useState(false);
+	const id = props.match.params.id;
+	const primary = green[500];
+	const userId = useSelector(state => state.userReducer.loggedInUser.id);
 
-  // useForm custom hook and set timeout custom hook
-  const { values, setValues, handleChange, handleSubmit } = useForm(
-    submitReply
-  );
-  // Fetches Auth0 token for axios call
-  const [token] = useGetToken();
+	// useForm custom hook and set timeout custom hook
+	const { values, setValues, handleChange, handleSubmit } = useForm(
+		submitReply
+	);
+	// Fetches Auth0 token for axios call
+	const [token] = useGetToken();
 
-  useEffect(() => {
-    // Fetch group related data
-    const fetchData = async () => {
-      if (token) {
-        const response = await axiosWithAuth([token]).get(`/posts/${id}`);
-        setPost(response.data.postLoaded);
-        console.log("data:", response.data.postLoaded);
-        setSubmitted(false);
-      }
-    };
-    fetchData();
-  }, [token, id, submitted]);
+	useEffect(() => {
+		// Fetch group related data
+		const fetchData = async () => {
+			if (token) {
+				const response = await axiosWithAuth([token]).get(`/posts/${id}`);
+				setPost(response.data.postLoaded);
+				console.log("data:", response.data.postLoaded);
+				setSubmitted(false);
+			}
+		};
+		fetchData();
+	}, [token, id, submitted]);
 
-  // callback function to handle submit
-  async function submitReply(e) {
-    const post = await axiosWithAuth([token]).post(`/replies/post/${id}`, {
-      user_id: userId,
-      post_id: id,
-      reply_content: values.reply_content
-    });
-    if (post.data.reply) {
-      setValues("");
-      setSubmitted(true);
-    }
-  }
+	// callback function to handle submit
+	async function submitReply(e) {
+		const post = await axiosWithAuth([token]).post(`/replies/post/${id}`, {
+			user_id: userId,
+			post_id: id,
+			reply_content: values.reply_content
+		});
+		if (post.data.reply) {
+			setValues("");
+			setSubmitted(true);
+		}
+	}
 
-  // Material UI
-  const useStyles = makeStyles(theme => ({
-    container: {
-      display: "flex",
-      flexWrap: "wrap"
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      backgroundColor: "white"
-    },
-    dense: {
-      marginTop: theme.spacing(2)
-    },
-    menu: {
-      width: 200
-    },
-    margin: {
-      margin: theme.spacing(1)
-    },
-    avatar: {
-      margin: 3,
-      width: 60,
-      height: 60
-    },
-    typography: {
-      fontSize: 13,
-      color: "black",
-      padding: 0
-    },
-    typography2: {
-      fontSize: 15,
-      color: "black",
-      fontWeight: "bold"
-    },
-    root: {
-      background: primary,
-      margin: theme.spacing(1)
-    }
-  }));
+	// Material UI
+	const useStyles = makeStyles(theme => ({
+		container: {
+			display: "flex",
+			flexWrap: "wrap"
+		},
+		textField: {
+			marginLeft: theme.spacing(1),
+			marginRight: theme.spacing(1),
+			backgroundColor: "white"
+		},
+		dense: {
+			marginTop: theme.spacing(2)
+		},
+		menu: {
+			width: 200
+		},
+		margin: {
+			margin: theme.spacing(1)
+		},
+		avatar: {
+			margin: 3,
+			width: 60,
+			height: 60
+		},
+		typography: {
+			fontSize: 13,
+			color: "black",
+			padding: 0
+		},
+		typography2: {
+			fontSize: 15,
+			color: "black",
+			fontWeight: "bold"
+		},
+		root: {
+			background: primary,
+			margin: theme.spacing(1)
+		}
+	}));
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  if (!post) {
-    return (
-      <Loader active size="large">
-        Loading
-      </Loader>
-    );
-  }
-  console.log("POST", post);
-  return (
-    <ReplyViewContainer>
-      <PostCard post={post} setSubmitted={setSubmitted} />
+	if (!post) {
+		return (
+			<Loader active size="large">
+				Loading
+			</Loader>
+		);
+	}
+	console.log("POST", post);
+	return (
+		<ReplyViewContainer>
+			<PostCard post={post} setSubmitted={setSubmitted} />
 
-      <ReplyCard1>
-        {post.replies.map(reply => {
-          return (
-            <ReplyCard
-              reply={reply}
-              setSubmitted={setSubmitted}
-              key={reply.id}
-            />
-          );
-        })}
-      </ReplyCard1>
+			<ReplyCardsContainer>
+				{post.replies.map(reply => {
+					return (
+						<ReplyCard
+							reply={reply}
+							setSubmitted={setSubmitted}
+							key={reply.id}
+						/>
+					);
+				})}
+			</ReplyCardsContainer>
 
-      <form className={"reply-form"} onSubmit={handleSubmit}>
-        <div className={"input-div"}>
-          <TextField
-            id="outlined-textarea"
-            label="Reply to post"
-            placeholder="Reply..."
-            multiline
-            fullWidth
-            className={classes.textField}
-            margin="normal"
-            variant="outlined"
-            onChange={handleChange}
-            name="reply_content"
-            value={values.reply_content || ""}
-          />
-        </div>
-        <Fab classes={{ root: classes.root }} type="submit" aria-label="Reply">
-          <AddIcon />
-        </Fab>
-      </form>
-    </ReplyViewContainer>
-  );
+			<form className={"reply-form"} onSubmit={handleSubmit}>
+				<div className={"input-div"}>
+					<TextField
+						id="outlined-textarea"
+						label="Reply to post"
+						placeholder="Reply..."
+						multiline
+						fullWidth
+						className={classes.textField}
+						margin="normal"
+						variant="outlined"
+						onChange={handleChange}
+						name="reply_content"
+						value={values.reply_content || ""}
+					/>
+				</div>
+				<Fab classes={{ root: classes.root }} type="submit" aria-label="Reply">
+					<AddIcon />
+				</Fab>
+			</form>
+		</ReplyViewContainer>
+	);
 };
 
 const ReplyViewContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #dee4e7;
-  min-height: 87vh;
-  justify-content: flex-start;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #dee4e7;
+	min-height: 87vh;
+	justify-content: flex-start;
 `;
-const ReplyCard1 = styled.div`
-  width: 100%;
-  height: 100%;
+const ReplyCardsContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	padding-bottom: 15%;
 `;
 
 export default ReplyContainer;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -5,60 +5,62 @@ export const ADD_GROUP = "ADD_GROUP";
 export const LEAVE_GROUP = "LEAVE_GROUP";
 
 const initialState = {
-  loggedInUser: "",
-  loggedInPosts: [],
-  loggedInGroups: [],
-  loggedInAllegiances: [
-    {
-      id: "1",
-      name: "Atlanta Braves",
-      image:
-        "https://cdn.bleacherreport.net/images/team_logos/328x328/atlanta_braves.png"
-    },
-    {
-      id: "2",
-      name: "San Francisco 49ers",
-      image:
-        "https://cdn.shopify.com/s/files/1/0257/6087/products/ca69f5096dd5a70ee8a7b86be09b148e.png?v=1558129992"
-    }
-  ],
-  error: ""
+	loggedInUser: "",
+	loggedInPosts: [],
+	loggedInGroups: [],
+	loggedInAllegiances: [
+		{
+			id: "1",
+			name: "Atlanta Braves",
+			image:
+				"https://cdn.bleacherreport.net/images/team_logos/328x328/atlanta_braves.png"
+		},
+		{
+			id: "2",
+			name: "San Francisco 49ers",
+			image:
+				"https://cdn.shopify.com/s/files/1/0257/6087/products/ca69f5096dd5a70ee8a7b86be09b148e.png?v=1558129992"
+		}
+	],
+	error: ""
 };
 
 export const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOGIN:
-      return {
-        ...state,
-        loggedInUser: action.payload.currentUser || action.payload.newUser,
-        loggedInGroups: action.payload.basicGroupInfo,
-        error: ""
-      };
-    case UPDATE_USER:
-      return {
-        ...state,
-        loggedInUser: action.payload,
-        error: ""
-      };
-    case GET_GROUPS:
-      return {
-        ...state,
-        loggedInGroups: action.payload,
-        error: ""
-      };
-    case ADD_GROUP:
-      return {
-        ...state,
-        loggedInGroups: [...state.loggedInGroups, action.payload]
-      };
-    case LEAVE_GROUP:
-      return {
-        ...state,
-        loggedInGroups: state.loggedInGroups.filter(
-          group => group.id !== action.payload
-        )
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case LOGIN:
+			return {
+				...state,
+				loggedInUser:
+					action.payload.userInfo.currentUser || action.payload.newUser,
+				loggedInGroups: action.payload.userInfo.basicGroupInfo,
+				loggedInAllegiances: action.payload.userInfo.basicAllegianceInfo,
+				error: ""
+			};
+		case UPDATE_USER:
+			return {
+				...state,
+				loggedInUser: action.payload,
+				error: ""
+			};
+		case GET_GROUPS:
+			return {
+				...state,
+				loggedInGroups: action.payload,
+				error: ""
+			};
+		case ADD_GROUP:
+			return {
+				...state,
+				loggedInGroups: [...state.loggedInGroups, action.payload]
+			};
+		case LEAVE_GROUP:
+			return {
+				...state,
+				loggedInGroups: state.loggedInGroups.filter(
+					group => group.id !== action.payload
+				)
+			};
+		default:
+			return state;
+	}
 };
