7dd488f2349460909db86413f0e5499cdf171886
Derek S <dschwan21@me.com>
2019-09-17 09:16:00 -0700

    started logic for displaying blocked view for private

--- src/components/group-page/GroupPage.js
+++ src/components/group-page/GroupPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import useGetToken from "../utils/useGetToken";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { VIEW_GROUP } from "../../reducers/navReducer";
 
 import GroupInfo from "./GroupInfo";
@@ -10,68 +10,90 @@ import PostsContainer from "../posts/PostsContainer";
 import styled from "styled-components";
 import { Paper } from "@material-ui/core";
 import { Loader } from "semantic-ui-react";
+import BlockedView from "../posts/BlockedView";
 
 const GroupPage = props => {
-	// Fetches Auth0 token for axios call
-	const [token] = useGetToken();
+  // Fetches Auth0 token for axios call
+  const [token] = useGetToken();
 
-	// Defines id to be group id from params
-	const id = props.match.params.id;
+  // Defines id to be group id from params
+  const id = props.match.params.id;
+  const userId = useSelector(state => state.userReducer.loggedInUser.id);
 
-	const [group, setGroup] = useState({});
-	const [allegiances, setAllegiances] = useState([]);
-	const [members, setMembers] = useState([]);
+  const [group, setGroup] = useState({});
+  const [allegiances, setAllegiances] = useState([]);
+  const [members, setMembers] = useState([]);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		// Fetch group related data
-		const fetchData = async () => {
-			if (token) {
-				try {
-					const response = await axiosWithAuth([token]).get(`/groups/${id}`);
-					setGroup(response.data.group);
-					setAllegiances(response.data.allegiances);
-					setMembers(response.data.members);
-					const groupId = response.data.group.id;
-					dispatch({ type: VIEW_GROUP, payload: groupId });
-				} catch {
-					dispatch({ type: VIEW_GROUP, payload: 0 });
-				}
-			}
-		};
-		fetchData();
-	}, [token, id, dispatch]);
+  useEffect(() => {
+    // Fetch group related data
+    const fetchData = async () => {
+      if (token) {
+        try {
+          const response = await axiosWithAuth([token]).get(`/groups/${id}`);
+          setGroup(response.data.group);
+          setAllegiances(response.data.allegiances);
+          setMembers(response.data.members);
+          const groupId = response.data.group.id;
+          dispatch({ type: VIEW_GROUP, payload: groupId });
+        } catch {
+          dispatch({ type: VIEW_GROUP, payload: 0 });
+        }
+      }
+    };
+    fetchData();
+  }, [token, id, dispatch]);
 
-	if (Object.keys(group).length === 0) {
-		return (
-			<Loader active size="large">
-				Loading
-      		</Loader>)
-	}
+  if (Object.keys(group).length === 0) {
+    return (
+      <Loader active size="large">
+        Loading
+      </Loader>
+    );
+  }
+  const currentUserType = members.find(member => member.id === userId);
+  console.log("USERTYPE", currentUserType);
 
-	return (
-		<GroupPageContainer>
-			<PaperContainer elevation={3}>
-				<GroupInfo group={group} members={members} allegiances={allegiances} />
-			</PaperContainer>
-			<PostsContainer groupId={group.id} members={members} />
-		</GroupPageContainer>
-	);
+  return (
+    <GroupPageContainer>
+      <PaperContainer elevation={3}>
+        <GroupInfo group={group} members={members} allegiances={allegiances} />
+      </PaperContainer>
+      <PostsContainer groupId={group.id} members={members} />
+      {/* {group.privacy_setting === "private" ||
+      ((group.privacy_setting === "hidden" &&
+        currentUserType &&
+        currentUserType.status !== "member") ||
+        currentUserType.status !== "admin") ? (
+        <BlockedView />
+      ) : (
+        <PostsContainer groupId={group.id} members={members} />
+	  )} */}
+      {/* {group.privacy_setting === "private" ||
+      ((group.privacy_setting === "hidden" &&
+        currentUserType.status !== "member") ||
+        currentUserType.status !== "admin") ? (
+        <BlockedView />
+      ) : (
+        <PostsContainer groupId={group.id} members={members} />
+      )} */}
+    </GroupPageContainer>
+  );
 };
 
 const GroupPageContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	width: 100%;
-	background-color: #dee4e7;
-	min-height: 87vh;
-	justify-content: flex-start;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  background-color: #dee4e7;
+  min-height: 87vh;
+  justify-content: flex-start;
 `;
 
 const PaperContainer = styled(Paper)`
-	margin-bottom: 5%;
+  margin-bottom: 5%;
 `;
 
 export default GroupPage;
