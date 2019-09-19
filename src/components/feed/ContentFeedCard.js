700d55db44e92fae66db0717eaf7cfbcebd041e5
Derek S <dschwan21@me.com>
2019-09-19 13:00:43 -0700

    created cards for the feed that contain only activity items involving posts or replies

--- src/components/feed/ContentFeedCard.js
+++ src/components/feed/ContentFeedCard.js
@@ -5,13 +5,14 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 345
+    width: "80%",
+    marginTop: 15,
+    marginBottom: 15
   }
 });
 
@@ -80,6 +81,9 @@ const ContentFeedCard = props => {
   );
 };
 
-const CardDiv = styled.div``;
+const CardDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default ContentFeedCard;
