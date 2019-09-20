7c8dea7f3986cd5ce0fb378b8fe64004f718304b
Derek S <dschwan21@me.com>
2019-09-20 11:03:10 -0700

    styled and fully functional content cards with conditional rendering

--- src/components/feed/ContentFeedCard.js
+++ src/components/feed/ContentFeedCard.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { createRef } from "react";
 import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import SmsOutlinedIcon from "@material-ui/icons/SmsOutlined";
+import ReplyIcon from "@material-ui/icons/Reply";
+import indigo from "@material-ui/core/colors/indigo";
+import { Link } from "react-router-dom";
+
+import "../../styled/Replies.scss";
 
 const useStyles = makeStyles({
   card: {
     width: "80%",
     marginTop: 15,
     marginBottom: 15
+  },
+  footer: {
+    flexDirection: "space-between"
+  },
+  groupHeader: {
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  avatar: {
+    marginRight: 15,
+    marginLeft: 15
+  },
+  groupAvatar: {
+    marginRight: 15,
+    marginLeft: 0,
+    height: 20,
+    width: 20
+  },
+  button: {
+    color: "white",
+    backgroundColor: "#1B4570"
+  },
+  content: {
+    color: "black",
+    fontSize: 18
   }
 });
 
 const ContentFeedCard = props => {
+  const primary = indigo[700];
   const classes = useStyles();
   const {
     id,
@@ -31,52 +65,71 @@ const ContentFeedCard = props => {
     first_name,
     last_name,
     image,
-    tag
+    tag,
+    group_image,
+    acronym
   } = props.activity;
+
+  const fullName =
+    first_name && last_name
+      ? first_name.charAt(0).toUpperCase() +
+        first_name.slice(1) +
+        " " +
+        last_name.charAt(0).toUpperCase() +
+        last_name.slice(1)
+      : null;
+
+  // defining post id for conditional render
+  let postId;
+  if (tag === "post") {
+    postId = id;
+  }
+  if (tag === "reply") {
+    postId = post_id;
+  }
   return (
     <CardDiv>
-      {tag === "post" && (
-        <Card className={classes.card}>
-          <CardActionArea>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {first_name} {last_name} created a post...
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {post_content}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              See Post
-            </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-      )}
+      <Card className={classes.card}>
+        <HeaderDiv>
+          <SmsOutlinedIcon className={classes.avatar} />
+          <p>
+            {" "}
+            {fullName} {tag === "post" && "created a post..."}{" "}
+            {tag === "reply" && "replied..."}{" "}
+          </p>
+        </HeaderDiv>
 
-      {tag === "reply" && (
-        <Card className={classes.card}>
-          <CardActionArea>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {first_name} {last_name} replied to someone...
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {reply_content}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              See Conversation
+        <CardActionArea>
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className={classes.content}
+            >
+              {tag === "post" && post_content}{" "}
+              {tag === "reply" && reply_content}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <Footer>
+          <Link to={`/post/${postId}`}>
+            <Button variant="contained" size="small" className={classes.button}>
+              {tag === "post" && "See Post"} {tag === "reply" && "See Reply"}
             </Button>
-          </CardActions>
-        </Card>
-      )}
+          </Link>
+          <Link to={`/group/${group_id}`}>
+            <GroupFooter>
+              <Avatar
+                aria-label="recipe"
+                className={classes.groupAvatar}
+                src={group_image}
+              />
+              <p>{acronym}</p>
+            </GroupFooter>
+          </Link>
+        </Footer>
+      </Card>
     </CardDiv>
   );
 };
@@ -84,6 +137,31 @@ const ContentFeedCard = props => {
 const CardDiv = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0.4rem 0;
+  text-align: left;
+  p {
+    color: gray;
+  }
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 6% 2% 4%;
+`;
+
+const GroupFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    color: black;
+  }
 `;
 
 export default ContentFeedCard;
