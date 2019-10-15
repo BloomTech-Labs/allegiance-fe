112edc2df7782dfe56295776e077c0eac4a83d3c
luis <mrluismartinezzz@gmail.com>
2019-10-15 15:21:55 -0700

    [create] create a post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,39 @@
-import { axiosWithAuth } from '../components/utils/axiosWithAuth';
-import * as actionTypes from './actionTypes';
+import { axiosWithAuth } from '../components/utils/axiosWithAuth'
+import * as actionTypes from './actionTypes'
 
 export const fetchGroupPosts = (token, id) => async dispatch => {
-    
-    if (token)
+  if (token)
     try {
       dispatch({ type: actionTypes.FETCH_POSTS_REQUEST })
       const posts = await axiosWithAuth([token]).get(`/posts/group/${id}`)
       const sortedPost = posts.data.postsLoaded.sort((a, b) => a.id - b.id)
       dispatch({ type: actionTypes.FETCH_POSTS_SUCCESS, payload: sortedPost })
-    } catch {
+    } catch (err) {
+      console.log(err)
       dispatch({ type: actionTypes.FETCH_POSTS_FAILURE })
     }
+}
+
+export const createGroupPost = (token, data) => async dispatch => {
+  const { userId, groupId, post_content } = data
+  if (token) {
+    try {
+      dispatch({ type: actionTypes.CREATE_POST_REQUEST })
+      const post = await axiosWithAuth([token]).post(
+        `/posts/group/${groupId}`,
+        {
+          user_id: userId,
+          group_id: groupId,
+          post_content: post_content,
+        }
+      )
+      dispatch({
+        type: actionTypes.CREATE_POST_SUCCESS,
+        payload: post.data.postResult,
+      })
+    } catch (err) {
+      console.log(err)
+      dispatch({ type: actionTypes.CREATE_POST_FAILURE })
+    }
+  }
 }
