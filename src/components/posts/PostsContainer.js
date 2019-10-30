183c3b416cc1c39d165bb30735c6d6aa723fe615
Zach Lasky <zacharylasky123@gmail.com>
2019-10-30 18:13:41 -0400

    changed group view when not signed in

--- src/components/posts/PostsContainer.js
+++ src/components/posts/PostsContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useAuth0 } from '../auth/react-auth0-wrapper'
 import useGetToken from '../utils/useGetToken'
 import styled from 'styled-components'
 import { Paper } from '@material-ui/core'
@@ -8,6 +9,7 @@ import PostForm from './PostForm'
 import PostCard from './PostCard'
 import { fetchGroupPosts } from 'actions'
 const PostsContainer = props => {
+  const { loginWithRedirect } = useAuth0()
   // Fetches Auth0 token for axios call
   const { posts } = props
   // Create ref and scrollToBottom function to align view upon entering tab and on new posts
@@ -28,30 +30,55 @@ const PostsContainer = props => {
   } else {
     membership = currentUserType.user_type
   }
-  return (
-    <PostsWrapper>
-      <PostListContainer>
-        {posts.length > 0 ? (
-          posts.map(post => {
-            return <PostCard post={post} key={post.id} />
-          })
-        ) : (
-          <PaperContainer elevation={20}>
-            <h2>Nobody has posted yet!</h2>
-          </PaperContainer>
+  // grab users signedIn state from redux
+  const userIn = useSelector(state => state.userReducer.loggedInUser)
+
+  // if user exists, return the page, else return the page with signIn button
+  if (userIn) {
+    return (
+      <PostsWrapper>
+        <PostListContainer>
+          {posts.length > 0 ? (
+            posts.map(post => {
+              return <PostCard post={post} key={post.id} />
+            })
+          ) : (
+            <PaperContainer elevation={20}>
+              <h2>Nobody has posted yet!</h2>
+            </PaperContainer>
+          )}
+        </PostListContainer>
+        <div ref={postsEndRef} />
+        {(membership === 'admin' || membership === 'member') && (
+          <PostForm groupId={props.groupId} scrollToBottom={scrollToBottom} />
         )}
-      </PostListContainer>
-      <div ref={postsEndRef} />
-      {(membership === 'admin' || membership === 'member') && (
-        <PostForm groupId={props.groupId} scrollToBottom={scrollToBottom} />
-      )}
-    </PostsWrapper>
-  )
+      </PostsWrapper>
+    )
+  } else {
+    return (
+      <PostsWrapper>
+        <PostListContainer>
+          {posts.length > 0 ? (
+            posts.map(post => {
+              return <PostCard post={post} key={post.id} />
+            })
+          ) : (
+            <JoinBtn onClick={() => loginWithRedirect({})}>
+              Log In To See Posts!
+            </JoinBtn>
+          )}
+        </PostListContainer>
+        <div ref={postsEndRef} />
+        {(membership === 'admin' || membership === 'member') && (
+          <PostForm groupId={props.groupId} scrollToBottom={scrollToBottom} />
+        )}
+      </PostsWrapper>
+    )
+  }
 }
 const PostListContainer = styled.div`
   width: 100%;
   display: flex;
-  border: 2px solid pink;
   flex-direction: column;
   align-items: center;
   background-color: #dee4e7;
@@ -62,5 +89,18 @@ const PostsWrapper = styled.div`
 `
 const PaperContainer = styled(Paper)`
   padding: 3.5rem;
+`
+const JoinBtn = styled.button`
+  height: 54px;
+  width: 192px;
+  border: none;
+  box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.5);
+  color: white;
+  background: #4483cd;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+  @media (max-width: 500px) {
+    width: 90vw;
+  }
 `
 export default PostsContainer
