02717b824e2dfde323657d8bd95f2893079dee3c
Sean Doyle <sdoylelambda@gmail.com>
2019-11-12 18:17:22 -0500

    Accordion refactored from class component to functional component.

--- src/components/nav/NavAccordion.js
+++ src/components/nav/NavAccordion.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Accordion, Icon } from 'semantic-ui-react'
 import GroupsNav from './GroupsNav'
 
-export default class NavAccordion extends Component {
-  state = { activeIndex: 1 }
-
-  handleClick = (e, titleProps) => {
+const NavAccordion = () => {
+  const [activeIndex, setActiveIndex] = useState({ activeIndex: 1 })
+  const handleClick = (e, titleProps) => {
     const { index } = titleProps
-    const { activeIndex } = this.state
-    const newIndex = activeIndex === index ? -1 : index
 
-    this.setState({ activeIndex: newIndex })
+    const newIndex = activeIndex.activeIndex === index ? -1 : index
+    setActiveIndex({ activeIndex: newIndex })
   }
 
-  render() {
-    const { activeIndex } = this.state
-
-    return (
-      <Accordion>
-        <Accordion.Title
-          active={activeIndex === 0}
-          index={0}
-          onClick={this.handleClick}
-        >
-          <Icon name='dropdown' />
-          My Groups
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 1}>
-          <GroupsNav />
-        </Accordion.Content>
-      </Accordion>
-    )
-  }
+  return (
+    <Accordion>
+      <Accordion.Title
+        active={activeIndex.activeIndex === 0}
+        index={0}
+        onClick={handleClick}
+      >
+        <Icon name='dropdown' />
+        My Groups
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex.activeIndex === 0}>
+        <GroupsNav />
+      </Accordion.Content>
+    </Accordion>
+  )
 }
+
+export default NavAccordion
