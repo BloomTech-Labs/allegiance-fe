e6422285cf8f3f6bb4f56cb4900f2f46c03218a7
Sean Doyle <sdoylelambda@gmail.com>
2019-11-13 19:23:08 -0500

    Styling added to cards in profile to view posts.

--- src/components/profile/ProfilePostsCard.js
+++ src/components/profile/ProfilePostsCard.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Card, CardContent } from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles'
 
 export default function ProfilePostsCards(props) {
-  console.log('Props from ProfilePostsCard:::::', props)
+  const classes = useStyles()
+
   return (
-    <ui>
-      <h1>
-        {props.post.first_name} {props.post.last_name}
-      </h1>
-      <h1>{props.post.post_content}</h1>
-    </ui>
+    <Card raised className={classes.card}>
+      <CardContent>
+        <ui>
+          <h1>{props.post.post_content}</h1>
+        </ui>
+      </CardContent>
+    </Card>
   )
 }
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    width: '95%',
+    maxWidth: '800px',
+    marginBottom: 20,
+  },
+}))
